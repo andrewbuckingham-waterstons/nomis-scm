6aa9a2c0d00ec33dd056d58aa1cc15f07da6962c
Andrew Buckingham <andrew.buckingham@waterstons.com>
2024-10-16 12:06:05 +0100

    Back to the start

--- Project/Calculations.cs
+++ Project/Calculations.cs
@@ -3,21 +3,22 @@ public class Calculations(IEnumerable<double> _data)
     public double TypicalDistanceTravelledToWork()
     {
         double score = 0.0;
-        int count = _data.Count();
-       
-        // TODO: 1. Change this to be the Median, rather than the Average, then move on to the Unit Test (Nithya)
-        score = _data
-                .OrderBy(x => x)
-                .Skip(count/2)
-                .First();
-
+        int count = 0;
         
+        // TODO: 1. Change this to be the Median, rather than the Average, then move on to the Unit Test (Nithya)
+        foreach (double value in _data)
+        {
+            score += value;
+            count++;
+        }
+
+        score /= count;
 
         return AsRoundedValue(score);
     }
     
     private static double AsRoundedValue(double original)
     {
-        return Math.Round(original, MidpointRounding.ToPositiveInfinity); // TODO: 2. Change this to use ToPositiveInfinity rounding (Peter)
+        return Math.Round(original, MidpointRounding.ToZero); // TODO: 2. Change this to use ToPositiveInfinity rounding (Peter)
     }
 }
